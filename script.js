4197aaef173413b45a5e999c5f7080eddd0f565d
Thiagod3 <thiago.vilela3@outlook.com>
2023-04-07 18:47:33 -0300

    sistem_acerto_commit

--- script.js
+++ script.js
@@ -70,25 +70,37 @@
     "fotos/yulla.jpg",
     ];
 
+    var semFotos;
+    var pasta = imagens;
+    console.log(pasta.length)
+    var nImagens = [] 
+    for(let i = 0; i < pasta.length;i++){
+        nImagens[i] = pasta[i].replace(`fotos/`, ``)
+    }
     
-var img = document.getElementById("random-img");
+
+
+    
+var img = document.getElementById("randomImg");
 img.src = "fotos/4lan.jpg";
+semFotos = "4lan.jpg"
 
 var jaFoi = [];
 
-var intervalId = setInterval(trocarImagem, 2000);
+var intervalId = setInterval(trocarImagem, 200000);
+
+var indice;
 
 function trocarImagem() {
-  var indice = Math.floor(Math.random() * imagens.length);
+  indice = Math.floor(Math.random() * imagens.length);
   img.src = imagens[indice];
+  semFotos = nImagens[indice];
 
   if (jaFoi.includes(imagens[indice])) {
     trocarImagem();
   } else {
     jaFoi.push(imagens[indice]);
   }
-
-  console.log(jaFoi);
 
   if(jaFoi.length == imagens.length){
     jaFoi = ["fotos/4lan.jpg"]
@@ -98,6 +110,48 @@ function trocarImagem() {
 function pararIntervalo() {
   clearInterval(intervalId);
 }
+var decremento = 10;
+let blurAtual = 50;
+let inputValue = document.getElementById("tentativa").value;
+
+function enviar(){
+    let inputValue = document.getElementById("tentativa").value;
   
+      if(!verificarString(inputValue, nImagens)){
+  
+          var novoBlur = blurAtual - decremento;
+          blurAtual = novoBlur;
+  
+          if (novoBlur < 0) {
+              blurAtual = 50;
+          }
+      img.style.filter = `blur(${novoBlur}px)`;
+  }
+}
+
+  function verificarString(inputValue, nImagens) {
+    var count = 0;
+    var imgSrc = semFotos;
+    for (let i = 0; i < imgSrc.length; i++) {
+
+      if (inputValue.includes(imgSrc[i])) {
+
+        for (let j = 0; j < inputValue.length; j++) {
+
+          if (inputValue[j] == imgSrc[i]) {
+            count++;
+          }
+        }
+      }
+    }
+    if (count >= 3) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+
   
   
